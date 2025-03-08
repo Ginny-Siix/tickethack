da308d6ccdf211067b0f5fcd089f01da22fbc06a
Mohamed Aouad <draouadmohamed@gmail.com>
2025-03-08 10:49:59 +0100

    add bookings functionality

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,32 +1,14 @@
 var express = require("express");
 var router = express.Router();
 require('../models/connection');
-const Trip = require('../models/trips');
 const Cart = require('../models/carts');
-const moment = require('moment');
-//var mongoose = require('mongoose');
 
-router.get("/", (req, res) => { //:id
-  const tempObject = { departure: "Lyon", arrival: "Paris", 
-    date: "2025-03-03T11:23:04.165+00:00", price: 137};
-  // req.query is replaced by tempObject
-  const date = new Date(tempObject.date);
-  // Définir la date de début et la date de fin de la journée du voyage
-  const startOfDay = new Date(date.setHours(0, 0, 0, 0)); // Début de la journée
-  const endOfDay = new Date(date.setHours(23, 59, 59, 999)); // Fin de la journée
-  //console.log("Conversion date ", date);
-  // Using params
-  //console.log(mongoose.Types.ObjectId.isValid('67c71793f2cecf2d3cdda3d8'));
-  //console.log(req.params);
-  //const {id}  = req.params;
+
+router.get("/", (req, res) => {
   Cart.find()
-    //departure: { $regex: new RegExp(tempObject.departure, 'i') },
-    //arrival: { $regex: new RegExp(tempObject.arrival, 'i') },
-    // Recherche entre le début et la fin de la journée
-    //date: { $gte: moment(date).startOf('day'), $lte: moment(date).endOf('day') }})
     .then(data => {
       console.log(data);
-      if (data.length > 0) { //data.length > 0
+      if (data.length > 0) { 
         return res.json( data );
       }
       else {
